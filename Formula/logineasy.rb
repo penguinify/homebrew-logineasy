4a87bf956c7468984581046bc17a6da9e1179f93
penguinify <penguinifyminecraft@gmail.com>
2024-08-02 13:49:50 -0700

    v0.1.1

--- Formula/logineasy.rb
+++ Formula/logineasy.rb
@@ -1,14 +1,17 @@
-ss Logineasy < Formula
+class Logineasy < Formula
   desc "A simple login utility"
   homepage "https://github.com/penguinify/logineasy"
   url "https://github.com/penguinify/logineasy/archive/refs/tags/v0.1.1.zip"
   sha256 "9f2d4c4718aaa636bd01409a49d24dd734493a58d068d521611cbe9ff8e76d35"
 
-  depends_on "make" => :build
+  depends_on "cmake" => :build
 
   def install
-    system "make"
-    bin.install "logineasy"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      bin.install "logineasy"
+    end
   end
 
   test do
