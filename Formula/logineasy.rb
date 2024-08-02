d0746f11d70d13a204189c1ac597f663a91c1d5f
penguinify <penguinifyminecraft@gmail.com>
2024-08-02 13:46:27 -0700

    v0.1.1

--- Formula/logineasy.rb
+++ Formula/logineasy.rb
@@ -1,22 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Logineasy < Formula
-  desc "cli tool to show ALL startup items/daemons (OSX)"
-  homepage ""
-  url "https://github.com/penguinify/logineasy/archive/refs/tags/v0.1.zip"
-  sha256 "230b89056d5957c212b7374e14602f46860dbfae6cfdba868ae2ef98c666659b"
-  license "GPL-3.0"
+ss Logineasy < Formula
+  desc "A simple login utility"
+  homepage "https://github.com/penguinify/logineasy"
+  url "https://github.com/penguinify/logineasy/archive/refs/tags/v0.1.1.zip"
+  sha256 "9f2d4c4718aaa636bd01409a49d24dd734493a58d068d521611cbe9ff8e76d35"
 
-  # depends_on "cmake" => :build
+  depends_on "make" => :build
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    #system "./configure", "--disable-silent-rules", *std_configure_args
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make"
+    bin.install "logineasy"
   end
 
   test do
+    system "#{bin}/logineasy", "--version"
   end
 end
