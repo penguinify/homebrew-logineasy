5a0109e76c99c24351f3ce969cae22c7e6afd2fc
penguinify <penguinifyminecraft@gmail.com>
2024-08-02 07:08:25 -0700

    v0.1.1

--- Formula/logineasy.rb
+++ Formula/logineasy.rb
@@ -4,7 +4,7 @@
 class Logineasy < Formula
   desc "cli tool to show ALL startup items/daemons (OSX)"
   homepage ""
-  url "https://github.com/penguinify/logineasy/releases/download/v0.1/logineasy.zip"
+  url "https://github.com/penguinify/logineasy/archive/refs/tags/v0.1.zip"
   sha256 "230b89056d5957c212b7374e14602f46860dbfae6cfdba868ae2ef98c666659b"
   license "GPL-3.0"
 
@@ -13,20 +13,10 @@ class Logineasy < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    #system "./configure", "--disable-silent-rules", *std_configure_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test logineasy`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
